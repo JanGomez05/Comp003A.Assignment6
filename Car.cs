7fc3031bcfa1856fdca2c368cbd124b87e872429
Jan Gomez <123139695+JanGomez05@users.noreply.github.com>
2023-02-25 10:11:46 -0800

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Comp003A.Assignment6
+{
+    /// <summary>
+    /// Car extends the Vehicle Class
+    /// </summary>
+    internal class Car : Vehicle
+    {
+
+        /***** constructors section *****/
+        // by default, a class has a default constructor that does not take any parameters
+        // therfore, you do not have to explicity create one
+
+
+        /***** methods section *****/
+        /// <summary>
+        /// Overide the base Vehicle's defintion for GetInfo()
+        /// </summary>
+        public override void GetInfo()
+        {
+            Console.WriteLine($"I'm a {nameof(Car)}");
+            Console.WriteLine("I don't want to disclose my information");
+       
+        }
+    }
+}
+
---
+++ Truck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Comp003A.Assignment6
+{
+    /// <summary>
+    /// Trucks extends the Vehicle class
+    /// </summary>
+    internal class Truck : Vehicle
+    {
+        /***** fields section *****/
+        private double _sizeOfTruckBed;
+
+
+        /***** constructors section *****/
+        /// <summary>
+        /// Constructor that takes 4 parameters: truckBrand, truckModel,
+        /// truckNumberOfWheels, and sizeOfTruckBed
+        /// It reuses the base class (Vehicle)'s constructor that takes 3 parameters:
+        /// brand, model, numberOfWheels
+        /// </summary>
+        /// <param name="truckBrand"></param>
+        /// <param name="truckModel"></param>
+        /// <param name="truckNumberOfWheels"></param>
+        /// <param name="sizeOfTruckBed"></param>
+        public Truck(string truckBrand, string truckModel, int truckNumberOfWheels,
+        double sizeOfTruckBed) : base(truckBrand, truckModel, truckNumberOfWheels)
+        {
+            double SizeOfTruckBed = sizeOfTruckBed;
+        }
+
+
+        /***** properties section *****/
+        public double SizeOfTruckbed
+        {
+            get { return _sizeOfTruckBed; }
+            set { _sizeOfTruckBed = value; }
+        }
+
+        /***** methods section *****/
+        /// <summary>
+        /// Override the bade Vehicle's defintion for GetInfo()
+        /// Calls the base Vehicle GetInfo() but adds on to it.
+        /// </summary>
+        public override void GetInfo()
+        {
+            Console.WriteLine($"Im a {nameof(Truck)}.");
+            base.GetInfo();
+            Console.WriteLine($"I also have a {SizeOfTruckbed} ft truck bed");
+
+        }
+    }
+}
+
